acb9fe5872eef241d8a24dbd15a2cfd53071528d
Sai Krishna <krishnayadavmaram18@gmail.com>
2022-05-04 10:57:06 +0530

    second

--- Java/Assesment/src/ArrayOfIntegers/ArrayOfIntegers.java
+++ Java/Assesment/src/ArrayOfIntegers/ArrayOfIntegers.java
@@ -4,38 +4,35 @@ import java.util.Arrays;
 import java.util.Scanner;
 
 public class ArrayOfIntegers {
+	int[] array = { 4, 3, 5, 6, 1, 2 };
+	boolean flag = true;
 
-	public int[] input = { 4, 3, 5, 1, 2 };
+	void method(int[] arr, boolean flag) {
+
+		for (int i = 0; i < arr.length; i++) { // Loop over java Array outer Loop use
+			for (int j = i + 1; j < arr.length; j++) { // Loop over java array
+				int num = 0;
+				// Temporary variable in order to compare.
+				if (arr[i] > arr[j]) { // compare outer loop object with inner loop
+					num = arr[i]; // if greater than swapping.
+					arr[i] = arr[j]; // Swapping code here.
+					arr[j] = num;
+				}
+			}
+		}
+		if (flag) {
+			for (int n : arr) {
+				System.out.println(n);
+			}
+		} else {
+			for (int reverse = arr.length - 1; reverse >= 0; reverse--) {
+				System.out.println(arr[reverse]);
+			}
+		}
+	}
 
 	public static void main(String[] args) {
-		int[] input = { 4, 3, 5, 1, 2 };
-		int temp;
-		Boolean flag;
-		if (flag = true) {
-			for (int i = 0; i < input.length; i++) {
-				for (int j = i + 1; j < input.length; j++) {
-					if (input[i] > input[j]) {
-						temp = input[i];
-						input[i] = input[j];
-						input[j] = temp;
-					}
-				}
-
-			}
-			System.out.println("Ascending order:" + Arrays.toString(input));
-		} else {
-			for (int i = 0; i < input.length; i++) {
-				for (int j = i + 1; j < input.length; j++) {
-					if (input[i] < input[j]) {
-						temp = input[i];
-						input[i] = input[j];
-						input[j] = temp;
-
-					}
-				}
-			}
-			System.out.println("Descending order:" + Arrays.toString(input));
-		}
-
+		ArrayOfIntegers arrayofintegers = new ArrayOfIntegers();
+		arrayofintegers.method(arrayofintegers.array, arrayofintegers.flag);
 	}
 }
